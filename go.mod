00d57dae5cc8b99c4cba49c1c556a8644b3eb37f
Q.S. Wang <wangqs_eclipse@yahoo.com>
2021-12-26 15:15:22 +0800

    Support JWT token generate and verification

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tuxiot/jwt
+
+go 1.16
+
+require github.com/golang-jwt/jwt v3.2.2+incompatible
---
+++ pkg/jwt/token.go
@@ -0,0 +1,42 @@
+package jwt
+
+import (
+	"fmt"
+
+	"github.com/golang-jwt/jwt"
+)
+
+// GenerateToken generates a new JWT token
+func GenerateToken(secrect []byte, issuer string) (token string, err error) {
+	// Create the Claims
+	claims := &jwt.StandardClaims{
+		Issuer: issuer,
+	}
+
+	tokenWithClaim := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	ss, err := tokenWithClaim.SignedString(secrect)
+	return ss, err
+}
+
+// VerifyToken verifies the JWT token with given secret key. HMAC only
+func VerifyToken(tokenString string, secretKey []byte) (err error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return secretKey, nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); !ok || !token.Valid {
+		return fmt.Errorf("invalid token")
+	}
+
+	return
+}
